c9bc56e023560e52ae1b9c9db31a8103282af81c
Pispache <pispachea@miumg.edu.gt>
2024-04-18 22:41:13 -0600

    Initial commit

---
+++ src/main/java/com/mycompany/proyecto2progra/NodoVehiculo.java
@@ -0,0 +1,98 @@
+package com.mycompany.proyecto2progra;
+
+public class NodoVehiculo {
+    String placa;
+    String color;
+    String linea;
+    int modelo;
+    String propietario;
+    NodoVehiculo arriba;
+    NodoVehiculo abajo;
+    NodoVehiculo izquierda;
+    NodoVehiculo derecha;
+
+    public NodoVehiculo(String placa, String color, String linea, int modelo, String propietario) {
+        this.placa = placa;
+        this.color = color;
+        this.linea = linea;
+        this.modelo = modelo;
+        this.propietario = propietario;
+        this.arriba = null;
+        this.abajo = null;
+        this.izquierda = null;
+        this.derecha = null;
+    } 
+    //getter y setters
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getLinea() {
+        return linea;
+    }
+
+    public void setLinea(String linea) {
+        this.linea = linea;
+    }
+
+    public int getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(int modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getPropietario() {
+        return propietario;
+    }
+
+    public void setPropietario(String propietario) {
+        this.propietario = propietario;
+    }
+
+    public NodoVehiculo getArriba() {
+        return arriba;
+    }
+
+    public void setArriba(NodoVehiculo arriba) {
+        this.arriba = arriba;
+    }
+
+    public NodoVehiculo getAbajo() {
+        return abajo;
+    }
+
+    public void setAbajo(NodoVehiculo abajo) {
+        this.abajo = abajo;
+    }
+
+    public NodoVehiculo getIzquierda() {
+        return izquierda;
+    }
+
+    public void setIzquierda(NodoVehiculo izquierda) {
+        this.izquierda = izquierda;
+    }
+
+    public NodoVehiculo getDerecha() {
+        return derecha;
+    }
+
+    public void setDerecha(NodoVehiculo derecha) {
+        this.derecha = derecha;
+    }
+    
+}
